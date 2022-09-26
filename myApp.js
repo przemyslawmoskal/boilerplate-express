b0d248a74103777350e0505e19831f669b90b5ad
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2022-09-26 22:51:17 +0200

    Uncomment app.get() function

--- myApp.js
+++ myApp.js
@@ -1,17 +1,14 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
-// app.get("/", function(req, res) {
-// 	res.send("Hello Express");
-// });
-// absolutePath = __dirname + "/views/index.html";
-// app.get("/", function(req, res) {
-// 	res.sendFile(absolutePath);
-// });
+
 path = __dirname + "/public";
+app.use(express.static(path));
 app.use("/public", express.static(path));
 
-
+absolutePath = __dirname + "/views/index.html";
+app.get("/", function(req, res) {
+	res.sendFile(absolutePath);
+});
 
 
 
