f3b158ce209c7502346a51940060e418058d76c2
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2022-09-29 14:47:33 +0200

    Changed app.get()

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
@@ -8,12 +10,24 @@ app.use("/public", express.static(path));
 // absolutePath = __dirname + "/views/index.html";
 // app.get("/", function(req, res) {
 // 	res.sendFile(absolutePath);
-//});
+// });
+
+// app.get("/json", function(req, res) {
+// 	res.json({
+//		"message": "Hello json"
+//	});
+// });
 
 app.get("/json", function(req, res) {
-	res.json({
-		"message": "Hello json"
-	});
+	if(process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({
+			"message": "HELLO JSON"
+		});
+	} else {
+		res.json({
+			"message": "Hello json"
+		});
+	}
 });
 
 
