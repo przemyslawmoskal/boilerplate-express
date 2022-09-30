8085215cf26fc5336a178d6107565b8d3fdd3e47
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2022-09-30 13:03:43 +0200

    Added app.use()

--- myApp.js
+++ myApp.js
@@ -18,19 +18,35 @@ app.get("/", function(req, res) {
 //	});
 // });
 
-app.get("/json", function(req, res) {
-	if(process.env.MESSAGE_STYLE === "uppercase") {
-		res.json({
-			"message": "HELLO JSON"
-		});
-	} else {
-		res.json({
-			"message": "Hello json"
-		});
-	}
+// app.get("/json", function(req, res) {
+	// if(process.env.MESSAGE_STYLE === "uppercase") {
+		// res.json({
+			// "message": "HELLO JSON"
+		// });
+	// } else {
+		// res.json({
+			// "message": "Hello json"
+		// });
+	// }
+// });
+
+// app.get("/json", function(req, res) {
+	// if(process.env.MESSAGE_STYLE === "uppercase") {
+		// res.json({
+			// "message": "HELLO JSON"
+		// });
+	// } else {
+		// res.json({
+			// "message": "Hello json"
+		// });
+	// }
+// });
+
+app.use((req, res, next) => {
+	let result = req.method + " " + req.path + " " + " - " + req.ip;
+	console.log(result);
+	next();
 });
-
-
 
 
 
